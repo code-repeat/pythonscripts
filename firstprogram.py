711fcc49655d0b7bed95496f7f431b1b001df3ba
Code Repeat <coderepeat44@gmail.com>
2020-06-10 14:58:50 -0300

    First program

---
+++ firstprogram.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name
+print("Hello world!")
+my_name = input("What's your name? ")  # ask for their name
+
+print(f"It's good to meet you {my_name}")
+print("The length of your name is:")
+print(len(my_name))
+
+my_age = input("How old are you?")  # ask for their age
+print(f"{my_name} you are {my_age} years old.")  # display name and age
