c7a6aae951e43b1b566327ed89368a280c54f0c0
Code Repeat <coderepeat44@gmail.com>
2020-06-11 21:13:54 -0300

    examples files added

---
+++ password.py
@@ -0,0 +1,8 @@
+name = input('Type a name: ')
+password = input('Type the password: ')
+if name == 'Mary':
+    print('Hello Mary')
+if password == 'swordfish':
+    print('Access granted.')
+else:
+    print('wrong password')
