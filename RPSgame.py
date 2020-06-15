e7f398a7088c706fb480a59412b916c9e8f298c0
Code Repeat <coderepeat44@gmail.com>
2020-06-15 20:18:13 -0300

    First version of the game

--- RPSgame.py
+++ RPSgame.py
@@ -1,22 +1,39 @@
 #  Welcome to my Rock, Paper, Scissor Game
 
-print('#'*30)
-print('-----ROCK PAPER SCISSOR v.1---')
-print('#'*30)
+while True:
+    print('#' * 30)
+    print('-----ROCK PAPER SCISSOR v.1---')
+    print('#' * 30)
 
-items = ["scissor", "paper", "rock"]
+    items = ["scissor", "paper", "rock"]
 
+    print("Enter player 1's choice: ")
+    p1_choice = input()
 
-print("Enter player 1's choice: ")
-p1_choice = input()
+    for i in range(10):
+        print("NO CHEATING!")
 
-for i in range(10):
-    print("NO CHEATING!")
+    print("Enter player 2's choice: ")
+    p2_choice = input()
 
-print("Enter player 2's choice: ")
-p2_choice = input()
+    if p1_choice == items[0] and p2_choice == items[1]:
+        print('Player 1 WINS!')
+    elif p1_choice == items[0] and p2_choice == items[2]:
+        print('Player 2 WINS!')
+    elif p1_choice == items[0] and p2_choice == items[0]:
+        print('DRAW!')
 
-if p1_choice == items[0] and p2_choice == items[1]:
-    print('Player 1 WINS!')
-elif p1_choice == items[2] and p2_choice == items[0]:
-    print('Player 2 WINS!')
+    # If player 1 choose PAPER
+    elif p1_choice == items[1] and p2_choice == items[0]:
+        print('Player 2 WINS!')
+    elif p1_choice == items[1] and p2_choice == items[1]:
+        print('DRAW!')
+    elif p1_choice == items[1] and p2_choice == items[2]:
+        print('Player 1 WINS!')
+    elif p1_choice == items[2] and p2_choice == items[0]:
+        print('Player 1 WINS')
+    elif p1_choice == items[2] and p2_choice == items[1]:
+        print('Player 2 WINS!')
+    elif p1_choice == items[2] and p2_choice == items[2]:
+        print('DRAW!')
+
