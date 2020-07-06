cfbda665a8bb5777ba0a34c76101fca0436dcbcc
Code Repeat <coderepeat44@gmail.com>
2020-07-06 19:25:41 -0300

    List Comprehension files

---
+++ listc02.py
@@ -0,0 +1,3 @@
+name = "Adam"
+upper_name = [char.upper() for char in name]
+print('*'.join(upper_name))
---
+++ listc03.py
@@ -0,0 +1,3 @@
+friends = ["ross", "monica", "rachel", "phoebes", "chandler", "joey"]
+friends_upper = [friend[0].upper() + friend[1:] for friend in friends]
+print(friends_upper)
