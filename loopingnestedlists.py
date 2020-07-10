a4749787201f2bad5dba64348c14ed41d5014726
coder-repeat <coderepeat44@gmail.com>
2020-07-10 17:09:47 -0300

    Nested Lists files

---
+++ loopingnestedlists.py
@@ -0,0 +1,11 @@
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for l in nested_list:
+    for val in l:
+        print(val)
+
+
+coords = [[10.423, 9.132], [37.212, -14.092], [21.367, 32.572]]
+for loc in coords:
+    for coord in loc:
+        print(coord)
+
